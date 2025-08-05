a350705545faeea1e903111616d0655334ec0a45
chuka <oluwachukkie@gmail.com>
2025-08-05 19:24:03 +0100

    Fix: corrected Investform import

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",                      // For local development
     "https://zentra-nu-woad.vercel.app",          // Vercel frontend
-    "http://zentravault.us/"           // Vercel frontend
+    "http://zentravault.us"           // Vercel frontend
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
